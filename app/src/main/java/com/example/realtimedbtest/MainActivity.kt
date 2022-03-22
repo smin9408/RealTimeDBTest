737bbc06d80124242a8f1ea1f0c9afb057b7a188
smin9408 <stmdaks888@naver.com>
2022-03-22 10:13:14 +0900

    제일 기본 형태의 데이터 기록 체험

--- app/src/main/java/com/example/realtimedbtest/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.realtimedbtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        DB 연결 -> 값 기록 연습.
+
+        val db = FirebaseDatabase.getInstance("https://realtimedbtest-1c829-default-rtdb.asia-southeast1.firebasedatabase.app/") // 싱가폴 DB주소 대입
+
+//        DB의 하위 항목 (Reference) 설정
+        val testRef = db.getReference("test")
+
+//        test 항목에, "Hello World!!" 기록
+        testRef.setValue("Hello World!!")
+
     }
 }
