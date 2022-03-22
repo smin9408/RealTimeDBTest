eccbfea74f8c65c9a16450622c80bed8bb5a1e49
smin9408 <stmdaks888@naver.com>
2022-03-22 10:20:46 +0900

    채팅 UI 설정  - DB연결 정보를 BaseActivity에서 상속

--- app/src/main/java/com/example/realtimedbtest/BaseActivity.kt
+++ app/src/main/java/com/example/realtimedbtest/BaseActivity.kt
@@ -3,10 +3,14 @@ package com.example.realtimedbtest
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
+
+//    멤버변수로 RealtimeDB에 연결
+    val realTimeDB = FirebaseDatabase.getInstance("https://realtimedbtest-1c829-default-rtdb.asia-southeast1.firebasedatabase.app/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/realtimedbtest/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest/MainActivity.kt
@@ -18,15 +18,7 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-//        DB 연결 -> 값 기록 연습.
 
-        val db = FirebaseDatabase.getInstance("https://realtimedbtest-1c829-default-rtdb.asia-southeast1.firebasedatabase.app/") // 싱가폴 DB주소 대입
-
-//        DB의 하위 항목 (Reference) 설정
-        val testRef = db.getReference("test")
-
-//        test 항목에, "Hello World!!" 기록
-        testRef.setValue("Hello World!!")
 
     }
 }
