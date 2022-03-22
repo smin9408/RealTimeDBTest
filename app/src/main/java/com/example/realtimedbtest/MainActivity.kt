970f524dea5b379430358d3f15d876d32ed7f873
smin9408 <stmdaks888@naver.com>
2022-03-22 11:03:06 +0900

    DB 변경시 => 마지막으로 추가된 사항을, ChattingData로 변환 / 목록에 추가  - 변경 항목 전체 중, 원하는 항목을 찾아가서 활용

--- app/src/main/java/com/example/realtimedbtest/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.realtimedbtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.realtimedbtest.datas.ChattingData
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -9,12 +10,15 @@ import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : BaseActivity() {
 
     var messageCount = 0L // DB에 저장된 채팅 갯수를 담을 변수. Long 타입으로 저장
 
-   override fun onCreate(savedInstanceState: Bundle?) {
+    val mChattingList = ArrayList<ChattingData>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -25,13 +29,22 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
 
 //        realTimeDB의 항목중, message 항목에 변화가 생길때.
-        realTimeDB.getReference("message").addValueEventListener( object : ValueEventListener{
+        realTimeDB.getReference("message").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
 //                파이어베이스의 DB내용 변경 => 이 함수를 실행시켜줌.
 
 //                snapshot변수 : 현재 변경된 상태 => 자녀가 몇개인지 추출.
                 messageCount = snapshot.childrenCount
+
+//                snapshot => 마지막 자녀(최신 채팅 메세지) 추출 => ChattingData로 변환 + 목록에 추가.
+
+                mChattingList.add(
+                    ChattingData(
+                        snapshot.children.last().child("content").value.toString(),
+                        snapshot.children.last().child("createdAt").value.toString()
+                    )
+                )
 
             }
 
@@ -46,14 +59,16 @@ class MainActivity : BaseActivity() {
 
 //            임시 : DB의 하위항목으로 => message 항목 => 0번 항목의 => content 항목 : 입력내용
 
-            realTimeDB.getReference("message").child(messageCount.toString()).child("content").setValue(inputContent)
+            realTimeDB.getReference("message").child(messageCount.toString()).child("content")
+                .setValue(inputContent)
 
 //            추가 기록 : 현재 시간값을 "2022년 3월 5일 오후 5:05" 양식으로 기록.
             val now = Calendar.getInstance()
             val sdf = SimpleDateFormat("yyyy년 M월 d일 a h:mm")
             val nowStr = sdf.format(now.time)
 
-            realTimeDB.getReference("message").child(messageCount.toString()).child("createdAt").setValue(nowStr)
+            realTimeDB.getReference("message").child(messageCount.toString()).child("createdAt")
+                .setValue(nowStr)
 
         }
     }
